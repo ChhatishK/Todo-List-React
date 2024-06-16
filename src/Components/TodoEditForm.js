9d4878cc92dea8c434a190dceed6977b2c21c4bf
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-06-16 14:29:24 +0530

    Update TodoEditForm.js

--- src/Components/TodoEditForm.js
+++ src/Components/TodoEditForm.js
@@ -10,13 +10,13 @@ function TodoEditForm({ editTask, task }) {
     };
 
     return (
-        <form className='flex w-full justify-around border p-1 rounded-md' onSubmit={handleSubmit}>
+        <form className='flex w-full justify-center items-center gap-4 border p-1 rounded-md' onSubmit={handleSubmit}>
             <input
                 type="text"
                 value={value}
                 onChange={e => setValue(e.target.value)}
                 placeholder="Edit todo"
-                className='rounded-md pl-2 w-[60%] focus:shadow-green-400'
+                className='rounded-md pl-2 w-[75%] focus:shadow-green-400'
             />
             <IoMdCheckmarkCircleOutline className=' cursor-pointer text-2xl' onClick={handleSubmit} color='blue' />
         </form>
