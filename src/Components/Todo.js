34e31d74b63fa5ced7665e88091ae02c3ca7d91e
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-06-16 14:26:14 +0530

    Update Todo.js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiEdit } from "react-icons/fi";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 function Todo({ task, toggleComplete, deleteTodo, editTodo }) {
     return (
@@ -11,8 +13,8 @@ function Todo({ task, toggleComplete, deleteTodo, editTodo }) {
                 {task.task}
             </p>
             <div className='flex gap-3 p-1'>
-                <button className='w-[80px] bg-green-400 hover:bg-green-600 transition-all duration-200 rounded-md hover:text-white' onClick={() => editTodo(task.id)}>Edit</button>
-                <button className='w-[80px] bg-red-400 hover:bg-red-600 transition-all duration-200 rounded-md hover:text-white' onClick={() => deleteTodo(task.id)}>Delete</button>
+                <FiEdit onClick={() => editTodo(task.id)} className='cursor-pointer text-2xl' color='white'  />
+                <RiDeleteBin6Line onClick={() => deleteTodo(task.id)} className='text-red cursor-pointer text-2xl' color='red'/>
             </div>
         </div>
     );
