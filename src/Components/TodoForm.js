4478160044457aed4977041c36acb08dabc931cb
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-06-16 14:32:40 +0530

    Update TodoForm.js

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -23,7 +23,7 @@ function TodoForm({ addTodo }) {
     return (
         <form className="flex flex-col gap-2 items-center" onSubmit={handleSubmit}>
             <input
-                autocomplete=off
+                autoComplete='off'
                 id='todo'
                 type="text"
                 value={task}
